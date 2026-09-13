78e4ea97da220835c5de810907895e1698c150f7
shahmeer-raza <shahmeerrazaarain@gmail.com>
2026-09-14 01:16:15 +0500

    feat: add login attempt limiter with 3-attempt restriction

--- login_page.py
+++ login_page.py
@@ -6,11 +6,16 @@ def login_page():
     print("--login page--")
     entered_id = input("Enter your account_id: ").strip()
     if user_exists(entered_id):
-        entered_password = input("Enter your password: ").strip()
         stored_password = get_user_password(entered_id)
-
-        if bcrypt.checkpw(entered_password.encode('utf-8'), stored_password.encode('utf-8')):
-            return True, entered_id
-        return False, None
+        for i in range(3):
+            entered_password = input("Enter your password: ").strip()
+            if bcrypt.checkpw(
+                entered_password.encode("utf-8"), stored_password.encode("utf-8")
+            ):
+                return True, entered_id
+            elif i < 2:
+                print("Wrong password, Try again!")
+                continue
+            return False, None
     else:
         return None, None
